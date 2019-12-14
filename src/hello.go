07567702fdc38faf95eb46b90f2f48a94a8a3d4c
Kozhemyak Nikolay <nikolay_kozhemyak@epam.com>
2019-12-14 21:04:58 +0600

    Going through the getting starged section

--- src/hello.go
+++ src/hello.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	//"math"
+)
 
+// my first function
+func add(x int, y int) int {
+	return x + y
+}
+
+// omit the type for two input variables
+func multipleVars(x, y string) (string, string) {
+	// return multiple results
+	return x, y
+}
+
+func main() {
+	// First call
+	fmt.Println("Hello World!", "Are you ready to \"Go\"?")
+
+	// Function 1 call
+	fmt.Println(add(13, 13))
+
+	// Function 2 call
+	a, b := multipleVars("One", "Two")
+	fmt.Println(a, b)
 }
