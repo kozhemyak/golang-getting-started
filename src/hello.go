1576c5723535d0e6b6e2f875f25b38cf74b1db91
Kozhemyak Nikolay <nikolay_kozhemyak@epam.com>
2019-12-14 21:17:12 +0600

    Continue learning go-lang with get-started docs

--- src/hello.go
+++ src/hello.go
@@ -5,6 +5,10 @@ import (
 	//"math"
 )
 
+// package level statement "var"
+var maxCount, minCount int
+var isVisible, isCreated bool = true, false
+
 // my first function
 func add(x int, y int) int {
 	return x + y
@@ -16,14 +20,29 @@ func multipleVars(x, y string) (string, string) {
 	return x, y
 }
 
+// Named return values
+func namedReturn(sum int) (x, y int) {
+	// function  level statement "var"
+	var maximum int = 15
+
+	sum = maximum
+
+	x, y = (5 + sum), (10 + sum)
+	// "naked" return
+	return
+}
+
 func main() {
 	// First call
 	fmt.Println("Hello World!", "Are you ready to \"Go\"?")
 
-	// Function 1 call
+	// Function 1: add
 	fmt.Println(add(13, 13))
 
-	// Function 2 call
+	// Function 2: multipleVars
 	a, b := multipleVars("One", "Two")
 	fmt.Println(a, b)
+
+	// Function 3:
+	fmt.Println(namedReturn(1986))
 }
